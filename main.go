7580845d51fbe70132a42f4b6853e95242431feb
Joshua Candia <joshuacandia74@gmail.com>
2023-11-07 23:39:15 -0300

    feat(Server/variables):resto func created)

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/JoshuaCandia/godesde0/variables"
 )
 
 func main(){
-	variables.ShowIntegers()
+	state,text := variables.ConvertToText(1500)
+	fmt.Println(state)
+	fmt.Println(text)
 }
