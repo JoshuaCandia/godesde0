dceec4504acee70eb16bd2194d3968bc24d43d57
Joshua Candia <joshuacandia74@gmail.com>
2023-11-07 21:21:35 -0300

    feat(Server):First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoshuaCandia/godesde0
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/JoshuaCandia/godesde0/variables"
+)
+
+func main(){
+	variables.ShowIntegers()
+}
