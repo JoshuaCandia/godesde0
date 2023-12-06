ef96536b0ed499925ea79af198878a7550ed19ad
Joshua Candia <joshuacandia74@gmail.com>
2023-12-06 00:09:12 -0300

    fix(Server): exercise 02 changed

--- exercises/exercise02.go
+++ exercises/exercise02.go
@@ -7,24 +7,25 @@ import (
 	"strconv"
 )
 
-var num int
-
 func Multiply() {
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
-		fmt.Println("Entry number: ")
-		scanner := bufio.NewScanner(os.Stdin)
-
+		fmt.Println("Enter a number:")
 		if scanner.Scan() {
-			num, err := strconv.Atoi(scanner.Text())
-
+			number, err := strconv.Atoi(scanner.Text())
 			if err != nil {
-				fmt.Println("Invalid number, try again")
-			} else {
-				for i := 1; i <= 10; i++ {
-					fmt.Println(num, "x", i, "=", num*i)
-				}
-				break
+				fmt.Println("Invalid input. Please enter a valid number.")
+				continue
 			}
+
+			for i := 1; i <= 10; i++ {
+				fmt.Printf("%d x %d = %d\n", number, i, number*i)
+			}
+			break
+		} else {
+			fmt.Println("Error reading input. Exiting.")
+			break
 		}
 	}
 }
